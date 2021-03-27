c831c894faf2e7eb25c88754d6dea62650817511
Jose Carmelo Ramirez Beltran <jcarmelo37@gmail.com>
2021-03-27 10:26:00 -0600

    Created backend setup for Node and Express

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import { Row, Col } from 'react-bootstrap';
 import Product from '../components/Product';
-import products from '../products';
 
-const getProductList = () => {
+const HomeScreen = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get('/api/products');
+
+      setProducts(data);
+    };
+
+    fetchProducts();
+  }, []);
+
+  return (
+    <>
+      <h1>Latest Products</h1>
+      <Row>{getProductList(products)}</Row>
+    </>
+  );
+};
+
+const getProductList = (products) => {
   return products.map((product) => {
     return (
       <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
@@ -11,15 +32,6 @@ const getProductList = () => {
       </Col>
     );
   });
-};
-
-const HomeScreen = () => {
-  return (
-    <>
-      <h1>Latest Products</h1>
-      <Row>{getProductList()}</Row>
-    </>
-  );
 };
 
 export default HomeScreen;
