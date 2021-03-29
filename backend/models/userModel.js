1941baefbc13f0bb400e734f0ecbb1e8e6335cc6
Jose Carmelo Ramirez Beltran <jcarmelo37@gmail.com>
2021-03-29 10:21:47 -0600

    After Section 7 - Added Back end User Authentication and Registration

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
 
 const userModel = {
   name: {
@@ -26,6 +27,21 @@ const options = {
 };
 
 const userSchema = mongoose.Schema(userModel, options);
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+
+  next();
+});
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 
 const User = mongoose.model('User', userSchema);
 
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,40 +1,13 @@
 import express from 'express';
-import asyncHandler from 'express-async-handler';
 
-import Product from '../models/productModel.js';
+import {
+  getProducts,
+  getProductById,
+} from '../controllers/productController.js';
 
 const router = express.Router();
 
-/**
- * @description Fetch all Products from DB
- * @route GET /api/products
- * @access Public
- */
-router.get(
-  '/',
-  asyncHandler(async (req, res) => {
-    const products = await Product.find({});
-    res.json(products);
-  })
-);
-
-/**
- * @description Fetch single Product from DB based on id
- * @route GET /api/products/:id
- * @access Public
- */
-router.get(
-  '/:id',
-  asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
-
-    if (!product) {
-      res.status(404);
-      throw new Error('Product not found');
-    }
-
-    res.json(product);
-  })
-);
+router.route('/').get(getProducts);
+router.route('/:id').get(getProductById);
 
 export default router;
