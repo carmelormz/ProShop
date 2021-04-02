2a69816ad3b8cc5bffe201b0e7289ac3404650b4
Jose Carmelo Ramirez Beltran <jcarmelo37@gmail.com>
2021-04-01 20:49:19 -0600

    Updated .gitignore to do not include build folder

--- backend/server.js
+++ backend/server.js
@@ -21,10 +21,6 @@ if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('API is running');
-});
-
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
@@ -35,6 +31,17 @@ app.get('/api/config/paypal', (req, res) =>
 );
 
 const __dirname = path.resolve();
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running...');
+  });
+}
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
 /* ERRORS HANDLERS */
